7b7415971e574f29836db253797191501629d642
agathiyana <agathiyana@nallas.com>
2025-03-20 15:27:23 +0530

    updated ctracker html file
    updated ctracker html file

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask, jsonify, request, redirect, url_for
+from flask import Flask, jsonify, request, redirect, url_for,render_template
 
 
 app = Flask(__name__)
 
 @app.route('/ctracker')
 def ctracker():
-    return open('templates/ctracker.html').read()  # Serve the main page
+    req_list = ['QA-Automation-Selenium-API-Jan', 'QA-Automation-Selenium-ETL-March', 'QA-Automation-Selenium-API-Mar', 'QA-Automation-Selenium-API-Feb']  
+    # return open('templates/ctracker.html').read()  # Serve the main page
+    return render_template('ctracker.html', req_list=req_list)
 
+
+@app.route('/requirements')
+def manage_requirements():
+    req_list = ['QA-Automation-Selenium-API-Jan', 'QA-Automation-Selenium-ETL-March', 'QA-Automation-Selenium-API-Mar', 'QA-Automation-Selenium-API-Feb']  
+    return render_template('requirements.html', req_list=req_list)
 
 
 if __name__ == '__main__':
